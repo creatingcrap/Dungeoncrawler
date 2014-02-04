7f38906add8e2e7c4b015e4a262ef329d0da90ae
83n38 <83nhendo@gmail.com>
2014-02-04 21:20:12 +1100

    added click detector for slot 1
    Now I'm going to get started on my Maths Homework!

---
+++ DungeonCrawler/Assets/ui_interact.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+public var collider: BoxCollider2D;
+function Start () {
+
+}
+
+function Update () {
+	if(Input.GetMouseButtonUp(0))
+	{
+		var ray : Ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		//Debug.DrawRay (ray.origin, ray.direction * 10, Color.yellow);
+		if (Physics.Raycast(ray)) {
+			Debug.Log("Clicked");
+		}
+	}
+}
